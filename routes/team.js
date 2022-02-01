fff7b5d0793183adb85ac68a10b1994bfcf3b51e
Nicholas Rising <nicholas.rising@gmail.com>
2022-02-01 11:22:51 -0500

    Randomize team name order on refresh on team page

--- routes/team.js
+++ routes/team.js
@@ -2,5 +2,12 @@ var express = require('express')
 app = module.exports = express()
 
 app.get('/team', function(req, res) {
-    res.render('team');
+    people = ['Brad', 'Gabe', 'Iggy', 'Nico', 'Onur']
+    for (var i = people.length - 1; i > 0; i--) {
+        var swap = Math.floor(Math.random() * (i + 1));
+        var temp = people[i];
+        people[i] = people[swap];
+        people[swap] = temp;
+    }
+    res.render('team', {people: people});
 });

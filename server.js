c6ff6fe13e85205ca82afce6e23662a7cd87bc99
Nicholas Rising <nicholas.rising@gmail.com>
2022-02-01 00:16:13 -0500

    Remove routing, and add "Meet the Team" page and CSS

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const PORT = 80
 var express = require('express')
 
 var app = express()
+app.use(express.static('public'))
 app.set('view engine', 'hbs')
 
-var indexRouter = require('./routes/index')
-app.use('/', indexRouter);
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/team', function(req, res) {
+    res.render('team');
+});
 
 var listener = app.listen(PORT, function() {
     console.log('Server started')

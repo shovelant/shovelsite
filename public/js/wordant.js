0e090e20ff4743eed433f904855862a98755d7e5
Nicholas Rising <nicholas.rising@gmail.com>
2022-02-05 01:08:22 -0500

    End Wordant when the player has won

--- public/js/wordant.js
+++ public/js/wordant.js
@@ -1,4 +1,5 @@
 const KEYBOARD_LAYOUT = [10, 9, 9]
+let won = false
 let words
 let target
 
@@ -9,7 +10,6 @@ loadWords().then((response) => {
 
 document.addEventListener('keyup', (event) => {
     let keyPressed = event.key.toLowerCase()
-    console.log(keyPressed)
     if (keyPressed == 'enter') {
         enter()
     } else if (keyPressed == 'backspace') {
@@ -24,54 +24,78 @@ async function loadWords() {
     return (await response.text()).split('\n')
 }
 
-function key(letter) {
-    const guesses = document.getElementById('guesses')
-    const guess = guesses.children[guesses.children.length - 1]
-    const index = guess.innerText.indexOf('_')
-    if (index != -1) {
-        guess.innerText = guess.innerText.slice(0, index) + letter.toLowerCase() + '_'.repeat(4 - index)
+function enter() {
+    if (!won) {
+        const guesses = document.getElementById('guesses')
+        const guess = guesses.children[guesses.children.length - 1]
+        const submitted = guess.innerText
+        if (!submitted.includes('_')) {
+            if (!words.has(submitted)) {
+                guess.classList.add('bad')
+            } else {
+                let status = []
+                let leftover = []
+                for (let i = 0; i < 5; i++) {
+                    if (target[i] == submitted[i]) {
+                        leftover.push('')
+                        status.push('right')
+                    } else {
+                        leftover.push(target[i])
+                        status.push('wrong')
+                    }
+                }
+                for (let i = 0; i < 5; i++) {
+                    if (status[i] == 'wrong') {
+                        let index = leftover.indexOf(submitted[i])
+                        if (index != -1) {
+                            leftover[index] = ''
+                            status[i] = 'contains'
+                        }
+                    }
+                }
+                guess.innerText = ''
+                won = true
+                for (let i = 0; i < 5; i++) {
+                    guess.innerHTML += `<span class="${status[i]}">${submitted[i]}</span>`
+                    if (status[i] != 'right') {
+                        won = false
+                    }
+                }
+                if (!won) {
+                    newGuess = document.createElement('div')
+                    newGuess.className = 'guess'
+                    newGuess.innerText = '_____'
+                    document.getElementById('guesses').appendChild(newGuess)
+                    if (Math.random() < 0.25) {
+                        target = shiftWord(target)
+                    }
+                    shuffleKeyboard()
+                }
+            }
+        }
     }
 }
 
-function enter() {
-    const guesses = document.getElementById('guesses')
-    const guess = guesses.children[guesses.children.length - 1]
-    const submitted = guess.innerText
-    if (!submitted.includes('_')) {
-        if (!words.has(submitted)) {
-            guess.classList.add('bad')
-        } else {
-            let status = []
-            let leftover = []
-            for (let i = 0; i < 5; i++) {
-                if (target[i] == submitted[i]) {
-                    leftover.push('')
-                    status.push('right')
-                } else {
-                    leftover.push(target[i])
-                    status.push('wrong')
-                }
-            }
-            for (let i = 0; i < 5; i++) {
-                if (status[i] == 'wrong') {
-                    let index = leftover.indexOf(submitted[i])
-                    if (index != -1) {
-                        leftover[index] = ''
-                        status[i] = 'contains'
-                    }
-                }
-            }
-            guess.innerText = ''
-            for (let i = 0; i < 5; i++) {
-                guess.innerHTML += `<span class="${status[i]}">${submitted[i]}</span>`
-            }
-            newGuess = document.createElement('div')
-            newGuess.className = 'guess'
-            newGuess.innerText = '_____'
-            document.getElementById('guesses').appendChild(newGuess)
-            shuffleKeyboard()
+function shiftWord(word) { // Very lazy approach
+    let newWord = randomShift(word)
+    let attempts = 0
+    while (!words.has(newWord)) {
+        newWord = randomShift(word)
+        attempts++
+        if (attempts > 25) {
+            return word
         }
     }
+    return newWord
+}
+
+function randomShift(word) {
+    const index = 4
+    let charCode = Math.floor(Math.random() * 25 + 97)
+    if (word.charCodeAt(index) == charCode) {
+        charCode = 122
+    }
+    return word.slice(0, index) + String.fromCharCode(charCode) + word.slice(index + 1)
 }
 
 function shuffleKeyboard() {
@@ -99,14 +123,27 @@ function shuffleKeyboard() {
 }
 
 function back() {
-    const guesses = document.getElementById('guesses')
-    const guess = guesses.children[guesses.children.length - 1]
-    let index = guess.innerText.indexOf('_')
-    if (index != 0) {
-        if (index == -1) {
-            index = 5
+    if (!won) {
+        const guesses = document.getElementById('guesses')
+        const guess = guesses.children[guesses.children.length - 1]
+        let index = guess.innerText.indexOf('_')
+        if (index != 0) {
+            if (index == -1) {
+                index = 5
+            }
+            guess.innerText = guess.innerText.slice(0, index - 1) + '_'.repeat(6 - index)
         }
-        guess.innerText = guess.innerText.slice(0, index - 1) + '_'.repeat(6 - index)
+        guess.classList.remove('bad')
     }
-    guess.classList.remove('bad')
+}
+
+function key(letter) {
+    if (!won) {
+        const guesses = document.getElementById('guesses')
+        const guess = guesses.children[guesses.children.length - 1]
+        const index = guess.innerText.indexOf('_')
+        if (index != -1) {
+            guess.innerText = guess.innerText.slice(0, index) + letter.toLowerCase() + '_'.repeat(4 - index)
+        }
+    }
 }

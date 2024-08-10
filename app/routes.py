923a6222ea50b45f4aebaaa655b5f873fcb1f7bb
Nico Rising <nico@nicorising.com>
2024-08-09 22:43:06 -0400

    Add ruff pre-commit hooks

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
--- config.py
+++ config.py
@@ -1,4 +1,2 @@
-import os
-
 class Config:
     pass
